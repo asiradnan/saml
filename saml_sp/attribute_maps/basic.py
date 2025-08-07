a9fdea004cc4aa9d76eaf9507b71c48121415f39
asiradnan <asiradnan23@gmail.com>
2025-08-07 20:44:58 +0600

    shurur nai sesh

---
+++ saml_sp/attribute_maps/basic.py
@@ -0,0 +1,15 @@
+MAP = {
+    "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:basic",
+    "fro": {
+        'uid': 'username',
+        'mail': 'email',
+        'cn': 'first_name',
+        'sn': 'last_name',
+    },
+    "to": {
+        'username': 'uid',
+        'email': 'mail',
+        'first_name': 'cn',
+        'last_name': 'sn',
+    }
+}
