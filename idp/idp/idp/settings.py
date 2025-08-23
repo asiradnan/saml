64e92c61b5330cd405f7cc98a543794a773e1200
asiradnan <asiradnan23@gmail.com>
2025-08-23 19:42:42 +0600

    allowed all hosts

--- idp/idp/idp/settings.py
+++ idp/idp/idp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9o7ks5d+dyku@bx0o-tom)8+_68do^vs2*h8n%3cs)14-h%^=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*','localhost', '127.0.0.1']
 
 
 # Application definition
--- sp/sp/sp/settings.py
+++ sp/sp/sp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4sgw@-8gxj$tfku@9ztp!+c!ixrt0)(-gl9ofzihncgd!ppzif
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
 
 # Application definition
